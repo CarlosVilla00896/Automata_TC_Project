e324a0c54fd3916dfacda8a3cccead5e3be3ef4a
Carlos Villanueva <prince.vill10@gmail.com>
2018-08-19 03:29:06 -0600

    Editada clase de archivos
    Añadida función para abrir archivos

--- src/Classes/Archivos.java
+++ src/Classes/Archivos.java
@@ -1,15 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Classes;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
  * @author PC
  */
 public class Archivos {
+    File file;
+    FileReader fr;
+    Scanner scanner;
+    String filename = "Test.txt";
     
+    public Archivos (){
+        
+    }
+    
+    public void abrirArchivo(){
+        try{
+            file = new File(filename);
+            fr = new FileReader(file);
+            scanner = new Scanner(fr);
+            scanner.nextLine();
+            
+            while(scanner.hasNextLine()){
+                //hacer funcion del automata que recibe como parametro la cadena que se acaba de leer del fichero
+            }
+            
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(null != fr){
+                    fr.close();
+                }
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+    }
 }
